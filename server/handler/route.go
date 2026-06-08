31f205151c121b60fccb9b6ebc11b4f311bbad5d
zjyl1994 <me@zjyl1994.com>
2026-06-08 14:58:58 +0800

    server: update API prefix

--- server/handler/route.go
+++ server/handler/route.go
@@ -3,5 +3,5 @@ package handler
 import "github.com/gin-gonic/gin"
 
 func RegisterRoute(r gin.IRoutes) {
-	r.POST("/notify/opencode", NotifyOpencodeHandler)
+	r.POST("/api/notify/opencode", NotifyOpencodeHandler)
 }
